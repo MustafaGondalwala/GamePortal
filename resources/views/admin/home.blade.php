c899d237d86b210d478f1b0734440082c5a75e90
Mustafa <mustafa@gmail.com>
2022-08-02 18:30:23 +0530

    first commit

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,10 @@
+@extends('admin.layout.app')
+@section('content')
+
+<main class="content">
+    <div class="main-content">
+        <div class="content-overlay"></div>
+        <div class="content-wrapper">
+        </div>
+</main>
+@endsection
